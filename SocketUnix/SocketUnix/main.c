e6aa793b135fea4300824fae5c49e6a5bdb043f5
giulio <giulio.milani99@gmail.com>
2020-10-30 09:32:03 +0100

    Creazione Iniziale

---
+++ SocketUnix/SocketUnix/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SocketUnix
+//
+//  Created by MacBook pro Retina on 30/10/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    fprintf(, <#const char *restrict, ...#>)
+    printf("Hello, World!\n");
+    return 0;
+}
