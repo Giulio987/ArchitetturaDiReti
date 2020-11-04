bc28c12463649da1d44c71d96a297c67d2ba49ec
giulio <giulio.milani99@gmail.com>
2020-11-04 11:06:03 +0100

    Aggiunta commenti e file compilati

--- SocketUnix/rcp_naive_unix/server-iterative.c
+++ SocketUnix/rcp_naive_unix/server-iterative.c
@@ -22,13 +22,15 @@ int main(void)
         /* Usa AF_INET per forzare solo IPv4, AF_INET6 per forzare solo IPv6 */
         hints.ai_family = AF_UNSPEC;
         hints.ai_socktype = SOCK_STREAM;
-        hints.ai_flags = AI_PASSIVE;
-
+        hints.ai_flags = AI_PASSIVE; // Permette alla gettaddrinfo di andare a funzionare in modalità passiva
+        //preparandoci la struttura dati da passare all bind
+        //Non proprio portabilissimo se si usa AF_UNSPEC-> BSD e Windows-> poiche la situazione migliore sarebbe
+        //di avere 2 socket una AF_INET e una AF_INET6-> molto piu complicato e servirebbe solo per avere compatibilità massima
         if ((err = getaddrinfo(NULL, "50001", &hints, &res)) != 0) {
                 fprintf(stderr, "Errore setup indirizzo bind: %s\n", gai_strerror(err));
                 exit(EXIT_FAILURE);
         }
-
+        //Oviiamente a lato server non si usa il fallback-> si aggancia a uno dei due protoclli senza dire niente(IPV6 senza saperlo)
         if ((sd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0){
                 perror("Errore in socket");
                 exit(EXIT_FAILURE);
@@ -42,7 +44,7 @@ int main(void)
         freeaddrinfo(res);
 
         /* trasforma in socket passiva d'ascolto */
-        if (listen(sd, SOMAXCONN) < 0) {
+        if (listen(sd, SOMAXCONN) < 0) { //SOMAXCONN è la lunghezza della coda
                 perror("Errore in listen");
                 exit(EXIT_FAILURE);
         }
@@ -51,59 +53,64 @@ int main(void)
         /* chdir("/var/local/files"); */
 
         for(;;) {
-                int fd, ns, nread;
-                struct sockaddr_storage client_address;
-                socklen_t fromlen;
+          int fd, ns, nread;
+          struct sockaddr_storage client_address; // struttura pensata proprio per accept-Z in grado
+          // di contenere tutte le strutture che ritornano
+          socklen_t fromlen;
 
-                fromlen = sizeof(client_address);
-                ns = accept(sd, (struct sockaddr *)&client_address, &fromlen);
-		/* ns = accept(sd, NULL, NULL); */
-		if (ns < 0) {
-			if (errno == EINTR) {
-				continue;
-			}
-			perror("Errore in accept");
-			exit(EXIT_FAILURE);
-		}
+          fromlen = sizeof(client_address);
+          //accept riceve (porta passiva, indirizzo di memoria su cui andrà a scrivere l'estremo lato client)
+          //ogni volta che la chiamo devi resetta fromlen
 
-                memset(buff, 0, sizeof(buff));
-                if ((nread = read(ns, buff, sizeof(buff)-1)) < 0) {
-			perror("Errore in read");
-			close(ns);
-			continue;
-                }
+          ns = accept(sd, (struct sockaddr *)&client_address, &fromlen);
+          //Se non ci interessa leggere l'indirizzo del client passo-> sd, NULL, NULL
+          /* ns = accept(sd, NULL, NULL); */
+          if (ns < 0) {
+            if (errno == EINTR) {
+              continue;
+            }
+            perror("Errore in accept");
+            exit(EXIT_FAILURE);
+          }
 
-                printf("Il client ha chiesto il file %s.\n", buff);
-                if ((fd = open(buff, O_RDONLY)) < 0) {
-                        printf("File non esiste.\n");
-                        if (write(ns, "N", 1) < 0) {
-				perror("Errore in write");
-				close(ns);
-				continue;
-			}
-                } else {
-                        int cont = 0;
+          memset(buff, 0, sizeof(buff));
+          //L'assunzione è che con una sola read si legga tutta la stringa(separata in pacchetti tcp)-> non si dovrebbe fare
+          if ((nread = read(ns, buff, sizeof(buff)-1)) < 0) {
+            perror("Errore in read");
+            close(ns);
+            continue;
+          }
+          //passo il buffer come stringa null terminated
+          printf("Il client ha chiesto il file %s.\n", buff);
+          if ((fd = open(buff, O_RDONLY)) < 0) {
+            printf("File non esiste.\n");
+            if (write(ns, "N", 1) < 0) {
+              perror("Errore in write");
+              close(ns);
+              continue;
+            }
+          } else {
+            int cont = 0;
 
-                        printf("File esiste, lo invio al client.\n");
-                        if (write(ns, "S", 1) < 0) {
-				perror("Errore in write");
-				close(ns);
-				continue;
-			}
-                        while ((nread = read(fd, buff, sizeof(buff))) > 0) {
-                                if (write_all(ns, buff, nread) < 0) {
-					perror("Errore in write");
-					close(ns);
-					continue;
-				}
-                                cont += nread;
-                        }
-                        printf("Copia eseguita di %d byte.\n", cont);
-                        close(fd);
-                }
+            printf("File esiste, lo invio al client.\n");
+            if (write(ns, "S", 1) < 0) {
+              perror("Errore in write");
+              close(ns);
+              continue;
+            }
+            while ((nread = read(fd, buff, sizeof(buff))) > 0) {
+              if (write_all(ns, buff, nread) < 0) {//Vogliamo essere sicuri che tutti i caratteri vengano scritti
+                perror("Errore in write");
                 close(ns);
+                continue;
+              }
+              cont += nread;
+            }
+            printf("Copia eseguita di %d byte.\n", cont);
+            close(fd);
+          }
+          close(ns);
         }
 
         return 0;
-}
-
+      }
