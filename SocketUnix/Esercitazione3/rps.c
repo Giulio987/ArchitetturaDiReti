9c2a5500f1f4dded1c8791e8c1f45adc30cd57dd
Giulio <zero_cool@OptilexsZeroCool.home-life.hub>
2020-12-04 10:48:16 +0100

    Finita soluzione naive esercizitazione 3-1

--- SocketUnix/Esercitazione3/rps.c
+++ SocketUnix/Esercitazione3/rps.c
@@ -7,59 +7,63 @@
 #include <unistd.h>
 #define MAXDIM 2048
 
-int main(int argc, char **argv){
-  int sd, err;
-  struct addrinfo hints, *ptr, *res;
-  char request[MAXDIM], response[2048];
-  if(argc < 3 | argc > 4){
-    fprintf(stderr, "Uso corretto: rps hostname porta <option>\n" );
-    exit(1);
-  }
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_socktype = AF_UNSPEC;
-  hints.ai_family = AF_INET;
+int main(int argc, char **argv)
+{
+	int sd, err, nread;
+	struct addrinfo hints, *ptr, *res;
+	char request[MAXDIM], response[2048];
+	if ( (argc < 3) | (argc> 4))
+	{
+		fprintf(stderr, "Uso corretto: rps hostname porta <option>\n");
+		exit(1);
+	}
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_socktype = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 
-  err = getaddrinfo(argv[1], argv[2], &hints, &res);
-  if(err < 0){
-    fprintf(stderr, "Impossibile recuperare informazioni\n" );
-    exit(1);
-  }
-  for(ptr = res; ptr != NULL; ptr = ptr->ai_next){
-    if((sd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol)) <0){
-      continue;
-    }
-    if (connect(sd, ptr->ai_addr, ptr->ai_addrlen) == 0){
-      break;
-    }
-    close(sd);
-  }
-  if(ptr == NULL){
-    fprintf(stderr, "Impossibile stabilire una Connessione\n");
-    exit(2);
-  }
-  freeaddrinfo(res);
+	err = getaddrinfo(argv[1], argv[2], &hints, &res);
+	if (err < 0)
+	{
+		fprintf(stderr, "Impossibile recuperare informazioni\n");
+		exit(1);
+	}
+	for (ptr = res; ptr != NULL; ptr = ptr->ai_next)
+	{
+		if ((sd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol)) < 0)
+		{
+			continue;
+		}
+		if (connect(sd, ptr->ai_addr, ptr->ai_addrlen) == 0)
+		{
+			break;
+		}
+		close(sd);
+	}
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Impossibile stabilire una Connessione\n");
+		exit(2);
+	}
+	freeaddrinfo(res);
 
-  snprintf(request, 2,  "%s\n", "ps");
+	sprintf(request, "%s", "request");
 
-  if(argc == 4){
-    snprintf(request, strlen(argv[3]),  "%s\n", argv[3]);
-  }
-  if(write(sd, request, strlen(request)) < 0){
-    perror("Write 1\n");
-    exit(3);
-  }
-  memset(response, 0, sizeof(response))
-  while((nread = read(sd, response, sizeof(response) - 1)) > 0){
-    fprintf(stdout, "%s\n", response );
-  }
-  
-  close(sd);
+	if (argc == 4)
+	{
+		snprintf(request, sizeof(argv[3]), "%s", argv[3]);
+	}
+	
+	if (write(sd, request, strlen(request)) < 0)
+	{
+		perror("Write 1\n");
+		exit(3);
+	}
+	memset(response, 0, sizeof(response));
+	while ((nread = read(sd, response, sizeof(response) - 1)) > 0){
+		fprintf(stdout, "%s\n", response);
+	}
 
+	close(sd);
 
-
-
-
-
-
-  return 0;
+	return 0;
 }
---
+++ SocketUnix/Esercitazione3/rpsServer-iterat.c
@@ -0,0 +1,132 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
+{
+    int sd, err, on;
+    struct addrinfo hints, *res;
+    typedef int pipe_t[2];
+    /* Controllo argomenti */
+    
+    if (argc != 2)
+    {
+        fprintf(stderr, "Sintassi: %s porta\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /* Preparo direttive getaddrinfo */
+    memset(&hints, 0, sizeof(hints));
+    /* Usa AF_INET per forzare solo IPv4, AF_INET6 per forzare solo IPv6 */
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    
+    if ((err = getaddrinfo(NULL, argv[1], &hints, &res)) < 0)
+    {
+        fprintf(stderr, "Errore recupero informazioni\n");
+        exit(1);
+    }
+    if ((sd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0)
+    {
+        perror("Creazione Socket\n");
+        exit(2);
+    }
+    on = 1;
+    if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
+    {
+        perror("Sockopt\n");
+        exit(3);
+    }
+    if (bind(sd, res->ai_addr, res->ai_addrlen) < 0)
+    {
+        perror("Bind\n");
+        exit(4);
+    }
+    freeaddrinfo(res);
+    if (listen(sd, SOMAXCONN) < 0)
+    {
+        perror("Listen\n");
+        exit(5);
+    }
+   
+                
+    for (;;)
+    {
+        char request[2048], response[4096];
+        int ns, nread, pid, status;
+        pipe_t p1ps;
+        ns = accept(sd, NULL, NULL);
+        
+        if (ns < 0)
+        {
+            if (errno == EINTR){
+                continue;
+            }
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        
+        memset(request, 0, sizeof(request));
+        if ((nread = read(ns, request, sizeof(request) - 1)) < 0)
+        {
+            perror("read");
+            close(ns);
+            continue;
+        }
+        
+        
+        if(pipe(p1ps) < 0){
+            perror("Pipe");
+            exit(4);
+        }
+        if ((pid = fork()) < 0)
+        {
+            perror("fork");
+        }
+        if (pid == 0)
+        { //figlio
+            //close(sd);
+            close(p1ps[0]);
+            close(1);
+            dup(p1ps[1]);
+            if (!strcmp(request, "request"))
+            {
+                execlp("ps", "ps", (char *)0);
+            }
+            else
+            {   
+                execlp("ps", "ps", request, (char *)0);
+            }
+            perror("execlp");
+            exit(7);
+        }
+        close(p1ps[1]);
+        memset(response, 0 , sizeof(response));
+        while ((nread = read(p1ps[0], response, sizeof(response) )) < 0)
+        {
+            perror("readPipe");
+            close(ns);
+            continue;
+        }
+        close(p1ps[0]);
+        wait(&status);
+        if (write(ns, response, strlen(response)) < 0)
+        {
+            perror("write");
+            close(ns);
+            continue;
+        }
+
+        /* Chiudo la socket attiva */
+        close(ns);
+    }
+    close(sd);
+    return 0;
+}
